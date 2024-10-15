072b2bf5dc14bab01df34750eb8c1a04cc790fa3
Yeray Navarro <yeraynp16@gmail.com>
2024-10-15 20:04:12 +0100

    Estudio Examen Sin Terminar

---
+++ src/activity.js
@@ -0,0 +1,15 @@
+const Service = require('./service')
+
+class Activity extends Service {
+  constructor (name, fixedCost = 0, variableCost = 0, assistance = 0) {
+    super(name)
+    this.fixedCost = fixedCost
+    this.variableCost = variableCost
+    this.assistance = assistance
+  }
+
+  calculateCost () {
+    return this.fixedCost + this.variableCost * this.assistance
+  }
+}
+module.exports = Activity
--- src/sport-center.js
+++ src/sport-center.js
@@ -1,4 +1,8 @@
+const Activity = require('./activity')
+const Service = require('./service')
+
 class SportCenter {
+  #services = []
   constructor (name, fee = 0, membership = 0) {
     if (name !== undefined) {
       this.name = name
@@ -9,6 +13,27 @@ class SportCenter {
 
   income () {
     return this.fee * this.membership
+  }
+
+  getServices () {
+    return this.#services
+  }
+
+  addService (...services) {
+    for (let i = 0; i < services.length; i++) {
+      const service = services[i]
+      if (service instanceof Service && !this.#services.includes(service)) this.#services.push(service)
+    }
+  }
+
+  removeService (service) {
+    if (this.#services.includes(service)) {
+      this.#services = this.#services.filter(s => s !== service)
+    }
+  }
+
+  getFacilities () {
+    return this.#services.filter(s => s instanceof Activity)
   }
 }
 module.exports = SportCenter
